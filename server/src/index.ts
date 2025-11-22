243c54c707e302bf65463634b8d411d633ce731e
Amodh Dhakal <amodhakal@gmail.com>
2025-11-22 13:32:58 -0500

    Added a logger middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
+app.use(morgan("combined"));
+
 app.listen(PORT, () => console.log(`On http://localhost:${PORT}`));
