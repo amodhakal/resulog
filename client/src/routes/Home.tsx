09a29a24ec97611057046d1d33883f6ce54a4791
Amodh Dhakal <amodhakal@gmail.com>
2025-11-22 14:54:51 -0500

    Setup react router and a basic home

---
+++ client/src/routes/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="text-3xl font-bold">Home</div>
+    </div>
+  );
+}
